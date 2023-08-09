de74e9e7b87b7b9df9d41aca80cb351f68b9c5c7
rizfadh <kikimuhammad69@gmail.com>
2023-08-09 16:44:22 +0700

    Some minor change

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -2,7 +2,7 @@ import { FaSun, FaMoon } from "react-icons/fa"
 
 export default function DarkModeButton({ darkModeValue, darkModeChange }) {
     return (
-        <div className="position-fixed z-3 bg-primary top-0 end-0 me-4 rounded-bottom-2 py-3 px-1 shadow">
+        <div className="position-fixed z-3 bg-primary top-0 end-0 me-5 rounded-bottom-2 py-3 px-1 shadow">
             <button
                 type="button"
                 className="d-flex btn text-light border-0 fs-2"
