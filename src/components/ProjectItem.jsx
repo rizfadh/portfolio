937616ee70cdd2c717f84df7ad46835f474f2954
rizfadh <kikimuhammad69@gmail.com>
2023-10-16 16:55:00 +0700

    edit avatar size

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,12 +1,17 @@
-export default function ProjectItem({imgUrl, title, body, link}) {
-    return(
-        <div className="col">
-            <div className="card bg-body-tertiary position-relative h-100 border-0 shadow-sm">
-                <img src={imgUrl} className="card-img-top" alt={title}/>
-                <div className="card-body">
-                    <p className="card-title fs-5">{title}</p>
-                    <p className="card-text">{body}</p>
-                    <a href={link} className="stretched-link" target="_blank" rel="noopener noreferrer"></a>
+export default function ProjectItem({ imgUrl, title, body, link }) {
+    return (
+        <div className='col'>
+            <div className='card bg-body-tertiary position-relative h-100 border-0 shadow-sm'>
+                <img src={imgUrl} className='card-img-top' alt={title} />
+                <div className='card-body'>
+                    <p className='card-title fs-5'>{title}</p>
+                    <p className='card-text'>{body}</p>
+                    <a
+                        href={link}
+                        className='stretched-link'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    ></a>
                 </div>
             </div>
         </div>
