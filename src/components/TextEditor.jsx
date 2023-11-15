499d50135bc6fff32fce3908e7b005b15327c8e6
rizfadh <kikimuhammad69@gmail.com>
2023-11-15 19:49:47 +0700

    migrate from tinymce to CKE

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,47 +1,115 @@
-import React, { useRef } from 'react'
-import { Editor } from '@tinymce/tinymce-react'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic'
+import { Essentials } from '@ckeditor/ckeditor5-essentials'
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat'
+import {
+    Bold,
+    Code,
+    Italic,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    Underline,
+} from '@ckeditor/ckeditor5-basic-styles'
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote'
+import { Heading } from '@ckeditor/ckeditor5-heading'
+import { Link } from '@ckeditor/ckeditor5-link'
+import { List } from '@ckeditor/ckeditor5-list'
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph'
+import { Alignment } from '@ckeditor/ckeditor5-alignment'
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table'
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block'
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent'
+import { Font } from '@ckeditor/ckeditor5-font'
+import { Highlight } from '@ckeditor/ckeditor5-highlight'
 
-function TextEditor({ init, name }) {
-    const editorRef = useRef(null)
+function TextEditor({ value, setValue }) {
+    const config = {
+        plugins: [
+            Essentials,
+            Autoformat,
+            Bold,
+            Italic,
+            Underline,
+            Strikethrough,
+            Superscript,
+            Subscript,
+            Code,
+            BlockQuote,
+            Heading,
+            Link,
+            List,
+            Paragraph,
+            Alignment,
+            Table,
+            TableToolbar,
+            CodeBlock,
+            Indent,
+            IndentBlock,
+            Font,
+            Highlight,
+        ],
+        heading: {
+            options: [
+                {
+                    model: 'paragraph',
+                    title: 'Paragraph',
+                    class: 'ck-heading_paragraph',
+                },
+                {
+                    model: 'heading2',
+                    view: 'h2',
+                    title: 'Heading 2',
+                    class: 'ck-heading_heading2',
+                },
+                {
+                    model: 'heading3',
+                    view: 'h3',
+                    title: 'Heading 3',
+                    class: 'ck-heading_heading3',
+                },
+            ],
+        },
+        table: {
+            contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+        },
+        toolbar: [
+            'undo',
+            'redo',
+            '|',
+            'heading',
+            'fontColor',
+            'fontBackgroundColor',
+            'link',
+            'alignment',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            {
+                label: 'More Formatting',
+                icon: 'text',
+                items: ['strikethrough', 'superscript', 'subscript', 'code'],
+            },
+            'highlight',
+            '|',
+            'bulletedList',
+            'numberedList',
+            'outdent',
+            'indent',
+            '|',
+            'blockQuote',
+            'insertTable',
+            'codeBlock',
+        ],
+    }
+
     return (
-        <Editor
-            apiKey={import.meta.env.VITE_TINYMCE_KEY}
-            onInit={(evt, editor) => (editorRef.current = editor)}
-            initialValue={init}
-            textareaName={name}
-            init={{
-                height: 500,
-                menubar: false,
-                plugins: [
-                    'advlist',
-                    'autolink',
-                    'lists',
-                    'link',
-                    'image',
-                    'charmap',
-                    'preview',
-                    'anchor',
-                    'searchreplace',
-                    'visualblocks',
-                    'code',
-                    'fullscreen',
-                    'insertdatetime',
-                    'media',
-                    'table',
-                    'code',
-                    'help',
-                    'wordcount',
-                ],
-                toolbar:
-                    'undo redo | blocks | ' +
-                    'bold italic forecolor link | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                content_style:
-                    'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-                block_formats:
-                    'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre',
-            }}
+        <CKEditor
+            editor={ClassicEditor}
+            config={config}
+            data={value}
+            onChange={(_, editor) => setValue(editor.getData())}
         />
     )
 }
