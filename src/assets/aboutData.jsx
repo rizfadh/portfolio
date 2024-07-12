d5f2fb0278f715070b865d8a744cca86e9dc009b
rizfadh <kikimuhammad69@gmail.com>
2024-07-12 07:53:52 +0700

    add skills

--- src/assets/aboutData.jsx
+++ src/assets/aboutData.jsx
@@ -1,45 +1,83 @@
 import { FaGithub, FaLinkedin, FaInstagram, FaAndroid } from "react-icons/fa"
-import { SiKotlin, SiJavascript, SiReact, SiFigma, SiAdobeillustrator, SiAdobephotoshop, SiFirebase } from "react-icons/si"
+import {
+  SiKotlin,
+  SiJavascript,
+  SiReact,
+  SiFigma,
+  SiAdobeillustrator,
+  SiAdobephotoshop,
+  SiFirebase,
+  SiNextdotjs,
+  SiTypescript,
+  SiPrisma,
+  SiTailwindcss,
+  SiShadcnui,
+  SiBootstrap,
+} from "react-icons/si"
 
 const contacts = [
-    {Icon: FaGithub, link: "https://github.com/rizfadh"},
-    {Icon: FaLinkedin, link: "https://www.linkedin.com/in/rizfadh"},
-    {Icon: FaInstagram, link: "https://www.instagram.com/rzy_fdhlh"},
+  { Icon: FaGithub, link: "https://github.com/rizfadh" },
+  { Icon: FaLinkedin, link: "https://www.linkedin.com/in/rizfadh" },
+  { Icon: FaInstagram, link: "https://www.instagram.com/rzy_fdhlh" },
 ]
 
 const skills = [
-    {
-        title: "Android",
-        Icon: FaAndroid
-    },
-    {
-        title: "Kotlin",
-        Icon: SiKotlin
-    },
-    {
-        title: "Javascript",
-        Icon: SiJavascript
-    },
-    {
-        title: "ReactJS",
-        Icon: SiReact
-    },
-    {
-        title: "Firebase",
-        Icon: SiFirebase
-    },
-    {
-        title: "Figma",
-        Icon: SiFigma
-    },
-    {
-        title: "Illustrator",
-        Icon: SiAdobeillustrator
-    },
-    {
-        title: "Photoshop",
-        Icon: SiAdobephotoshop
-    }
+  {
+    title: "NextJS",
+    Icon: SiNextdotjs,
+  },
+  {
+    title: "TypeScript",
+    Icon: SiTypescript,
+  },
+  {
+    title: "Prisma",
+    Icon: SiPrisma,
+  },
+  {
+    title: "Android",
+    Icon: FaAndroid,
+  },
+  {
+    title: "Kotlin",
+    Icon: SiKotlin,
+  },
+  {
+    title: "Javascript",
+    Icon: SiJavascript,
+  },
+  {
+    title: "ReactJS",
+    Icon: SiReact,
+  },
+  {
+    title: "Firebase",
+    Icon: SiFirebase,
+  },
+  {
+    title: "TailwindCSS",
+    Icon: SiTailwindcss,
+  },
+  {
+    title: "ShadcnUI",
+    Icon: SiShadcnui,
+  },
+  {
+    title: "Bootstrap",
+    Icon: SiBootstrap,
+  },
+  {
+    title: "Figma",
+    Icon: SiFigma,
+  },
+  {
+    title: "Illustrator",
+    Icon: SiAdobeillustrator,
+  },
+  {
+    title: "Photoshop",
+    Icon: SiAdobephotoshop,
+  },
 ]
 
 export { contacts, skills }
